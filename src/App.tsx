fb426244fdbfc80825f12d33eee09755efb6cb3e
Ryan Walker <ryandanielwalker@gmail.com>
2021-08-07 12:26:42 -0700

    Create QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+//components
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>Start</button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
